73530c483a23b82e0a4d8b3e723afaa97da1d95b
sensaj77 <wtpsawicki85@gmail.com>
2015-07-30 13:44:55 +0200

    Update testingWysiwyg.js

--- testingWysiwyg.js
+++ testingWysiwyg.js
@@ -19,31 +19,7 @@ describe("A test suite", function() {
 describe("wyswig-editor", function() {
    it('should add url on selected text', function() {
 
-    /*document.execCommand('selectall',false, null);
-
-    function simulateClick() {
-  var event = new MouseEvent('click', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': false
-  });
-  
-};
-simulateClick();
-$(".link-input").append("www.google.com");
- function simulateClick2() {
-  var event = new MouseEvent('click', {
-    'view': window,
-    'bubbles': true,
-    'cancelable': false
-  });
-  
-};
-simulateClick2();
-var addedLink= document.getElementById("contenteditable").innerHTML;
-
-    console.log("addedLink should be "+addedLink);*/
-
+    
     $(document).ready(function(){
       document.execCommand('selectall',false, null);
     $("button.icon-link").click();
@@ -100,14 +76,7 @@ describe( "wyswig-editor", function() {
     
 
 
-/*var yellow = document.getElementById("contenteditable").innerHTML;
-console.log(document);
-console.log(JSON.stringify(document));
-console.log(JSON.stringify(document.createRange));
-console.log(JSON.stringify(typeof document.createRange));
-console.log(typeof document.createRange);
 
-console.log(navigator.userAgent);*/
 
 
 document.execCommand('inserthtml',false," Yo ");
@@ -139,65 +108,46 @@ console.log( contentChanged);
 
 
 
-    /*  function getSelectionHtml() 
-{
-    var html = "";
-
-    if (typeof window.getSelection != "undefined") 
-        {
-        var sel = window.getSelection();
-        if (sel.rangeCount) 
-            {
-            var container = document.createElement("div");
-            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                container.appendChild(sel.getRangeAt(i).cloneContents());
-            }
-            html = container.innerHTML;
-        }
-    } else if (typeof document.selection != "undefined") {
-        if (document.selection.type == "Text") {
-            html = document.selection.createRange().htmlText;
-        }
-    }
-    return html;
-
-}
-var storeSelection = getSelectionHtml();
-console.log("XXXXXXXXXXXX " + storeSelection);*/
-
-/*
- document.execCommand('selectall',false, null);
-var selObj = document.getSelection(); // RETURNED SELECTION AS AN OBJECT?
-var selectedText = selObj.toString(); // Converted to a string
-console.log("NEWEST "+selectedText);
-var range = document.createRange();
-*/
-
-
-/*range.setStart(startNode, startOffset);
-range.setEnd(endNode, endOffset);*/
-    console.log("0000000000000000000000000");
+    
+   
+var element = document.querySelector("div");
     
 
-    var docRange = document.createRange();
-    var textNode = document.getElementById("contenteditable");
-    console.log(">>>>>>",textNode.innerHTML);
 
-    console.log(1);
-    var startOffset = 2;
-    console.log("######", textNode.firstChild);
-    docRange.setStart(textNode, startOffset);
-    console.log(2);
 
-    var endOffset = document.getElementsByTagName("p").length;
-    console.log("$$$$$$222222", endOffset);
-    docRange.setEnd(textNode, endOffset);
-    console.log(3);
+var setSelectionRange = function(element, start, end) {
+    var rng = document.createRange(),
+        sel = getSelection(),
+        n, o = 0,
+        tw = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, null);
+    while (n = tw.nextNode()) {
+        o += n.nodeValue.length;
+        if (o > start) {
+            rng.setStart(n, n.nodeValue.length + start - o);
+            start = Infinity;
+        }
+        if (o >= end) {
+            rng.setEnd(n, n.nodeValue.length + end - o);
+            break;
+        }
+    }
+    sel.removeAllRanges();
+    sel.addRange(rng);
+};
 
-    var selObj  = document.getSelection();
+setSelectionRange(element, 3, 15);
+
+
+
+
+
+
+    var selObj = window.getSelection();
     var selectedText = selObj.toString();
 
-    console.log("Neweset " + selectedText);
+    console.log("Newest " + selectedText)
+
+
     assert.equal(1,1);
 
   });
